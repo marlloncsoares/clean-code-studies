2d8baa2adba8aec24dce2b7439acefa40ece2e80
marlloncsoares <marlonmcs@hotmail.com>
2022-03-08 19:19:02 -0300

    feat: adiciona exemplos de nomes significativos

---
+++ src/nomes_significativos/exemplo1.js
@@ -0,0 +1,31 @@
+// Define o tempo decorrido em dias
+const d = 0;
+// Por que existe? NÃO SEI
+// O que faz? NÃO SEI
+// E como é usado? como uma constante númerica
+
+// Nomes melhores
+const tempoDecorridoEmDias = 0;
+const elapseTimeInDays = 0;
+// Por que existe? Para armazenar o tempo decorrido em dias
+// O que faz? Armazena valor númerico que representa o tempo decorrido em dias
+// E como é usado? como uma constante númerica
+
+const diasDesdeACriacao = 0;
+const daysSinceCreation = 0;
+// Por que existe? Para armazenar o tempo decorrido desde a criação
+// O que faz? Armazena valor númerico que representa o tempo decorrido desde a criação.
+// E como é usado? como uma constante númerica
+
+const diasDesdeAModification = 0;
+const daysSinceModification = 0;
+// Por que existe? Para armazenar os dias desde a modificação
+// O que faz? Armazena um valor númerico que representa os dias desde a modificação
+// E como é usado? como uma constante númerica
+
+
+const idadeDoArquivoEmDias = 0;
+const fileAgeInDays = 0;
+// Por que existe? Para armazenar a idade do arquivo em dias
+// O que faz? Armazena um valor númerico que representa a idade do arquivo em dias
+// E como é usado? como uma constante númerica
